d4c18ce168ffb546eaf1dcef7b1c251cf1bacbac
aoi20031020 <aoi20031020@gmail.com>
2025-02-08 16:52:05 +0900

    修正

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api'); // ここを追加
+
   // Vercel環境ではデフォルトのポートを使用し、環境変数がなければ3000番を使用
   const port = process.env.PORT || 3000;
   await app.listen(port);
