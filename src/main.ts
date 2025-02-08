9b1cc1fd350ed1831a4ee227aeb522157b02068b
aoi20031020 <aoi20031020@gmail.com>
2025-02-08 16:45:45 +0900

    mainを修正

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Vercel環境ではデフォルトのポートを使用し、環境変数がなければ3000番を使用
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
