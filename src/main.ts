4d20cfed6e8d7041d7141766e01802b2c6884822
aoi20031020 <aoi20031020@gmail.com>
2025-02-20 00:40:42 +0900

    cors設定

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'https://gdsc-membership-front.vercel.app', // フロントエンドのURL
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true, // 認証情報が必要ならtrue
+  });
   await app.listen(3000);
   console.log(`Application is running on: http://localhost:3000`);
 }
